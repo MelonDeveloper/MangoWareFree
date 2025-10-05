32b9547e3c571596459cbddcb1599b7365d9425d
Nick <nikita10020045@gmail.com>
2025-10-05 14:15:37 +0300

    Update MangoWare.lua

--- MangoWare.lua
+++ MangoWare.lua
@@ -1,39 +1,5 @@
-local InsertService = game:GetService("InsertService")
 
 local ReGui = loadstring(game:HttpGet('https://raw.githubusercontent.com/depthso/Dear-ReGui/refs/heads/main/ReGui.lua'))()
-
---// Declare the Prefabs asset
-
-
-
-
-
-ReGui:init()
-ReGui:DefineTheme("Orange", {
-	TitleAlign = Enum.TextXAlignment.Center,
-	TextDisabled = Color3.fromRGB(0,128,0),
-	Text = Color3.fromRGB(0,128,0),
-	
-	FrameBg = Color3.fromRGB(225, 142, 70),
-	FrameBgTransparency = 0.4,
-	FrameBgActive = Color3.fromRGB(240, 121, 16),
-	FrameBgTransparencyActive = 0.4,
-	
-	CheckMark = Color3.fromRGB(0,100,0),
-	SliderGrab = Color3.fromRGB(0,100,0),
-	ButtonsBg = Color3.fromRGB(208, 63, 10),
-	CollapsingHeaderBg = Color3.fromRGB(208, 63, 10),
-	CollapsingHeaderText = Color3.fromRGB(0,128,0),
-	RadioButtonHoveredBg = Color3.fromRGB(208, 63, 10),
-	
-	WindowBg = Color3.fromRGB(255,165,0),
-	TitleBarBg = Color3.fromRGB(249,62,62),
-	TitleBarBgActive = Color3.fromRGB(249,62,62),
-	
-	Border = Color3.fromRGB(0,128,0),
-	ResizeGrab = Color3.fromRGB(0,100,0),
-	RegionBgTransparency = 1,
-})
 
 local MHMainWindow = ReGui:Window({
 	Title = "Mangohub Testnet",
